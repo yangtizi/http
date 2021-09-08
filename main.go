d895116157d1741566931e291820800a6a3a61b2
warrially <mail@wary.wang>
2021-09-08 09:31:53 +0800

    demo

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
-	"github.com/yangtizi/go/fast"
+	"fmt"
 
 	"github.com/valyala/fasthttp"
+	"github.com/yangtizi/http/fast"
+	"github.com/yangtizi/http/post"
 )
 
 func main() {
-	fasthttpStartDemo()
+	// fasthttpStartDemo()
+	postDemo()
+	// post.Default.URL("http://127.0.0.1:1234/ping").AddHeader("time", time.Now().String())
+
+}
+
+func postDemo() {
+
+	post.Default.URL(
+		"https://www.qq.com/help/player/create").TLS(
+		"CNY.pem", "CNY.key").SetHeader(
+		"X_ENTITY_KEY", "12897389719283")
+	resp, err := post.Default.String("")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("返回结果", string(resp))
 }
 
 // 启动fast服务器
